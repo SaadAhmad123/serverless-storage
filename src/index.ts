fac5b6b787e36c56e7cbb8b81d03454f3557f6c7
Saad Ahmad <saadkwi12@hotmail.com>
2023-12-24 22:52:32 +1100

    update

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { IStorageManager, ILockingManager } from "./storage_manager";
+import LockableStorageManager, { ILockableStorageManager, LockableStorageMangerInput } from "./storage_manager/lockable_storage_manager";
+import DynamoLockingManager from "./storage_manager/dynamo_locking_manager";
+import S3StorageManager from "./storage_manager/s3_storage_manager";
+import LocalFileStorageManager from "./storage_manager/local_file_storage_manager";
+
+export {
+    IStorageManager,
+    ILockableStorageManager,
+    ILockingManager,
+    LockableStorageManager,
+    DynamoLockingManager,
+    S3StorageManager,
+    LocalFileStorageManager
+}
